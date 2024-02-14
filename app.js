481fed6b3024521d8c7f0c889673b62ef50a8985
Marios <marioqquelcca1998@gmail.com>
2024-02-13 22:11:25 -0500

    cambios actualizados

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'Bienvenido al mundo dev'
+
+let parrafo = document.querySelector('p')
+parrafo.innerHTML = 'Ingrese un numero entre el 1 al 10'
+
+function intentoDeUsuario(){
+    alert("click desde el boton")
+}
