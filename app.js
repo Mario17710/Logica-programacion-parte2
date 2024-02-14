dbd44b921c6105cae749e17cbf718f112fd51d1c
Marios <marioqquelcca1998@gmail.com>
2024-02-13 23:29:43 -0500

    actualizacion del js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Bienvenido al mundo dev'
-
-let parrafo = document.querySelector('p')
-parrafo.innerHTML = 'Ingrese un numero entre el 1 al 10'
+// creamos la funcion donde creamos las variables
+function asignarTextoElemento(elemento, texto){
+    let elementohtml = document.querySelector(elemento);
+    elementohtml.innerHTML = texto;
+}
+// llamamos a la funcion y modificamos el comportamiento de los elementos
+asignarTextoElemento('h1','Juego del numero secreto');
+asignarTextoElemento('p','Ingrese un numero del 1 al 10');
 
 function intentoDeUsuario(){
     alert("click desde el boton")
