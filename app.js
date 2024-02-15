cb501bfcd79506145a03f1893d05481dcc005577
Marios <marioqquelcca1998@gmail.com>
2024-02-15 18:28:00 -0500

    cambios

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// variable global
+let numeroaleatorio = generarnumeroaleatorio();
 // creamos la funcion donde creamos las variables
 function asignarTextoElemento(elemento, texto){
     let elementohtml = document.querySelector(elemento);
@@ -8,5 +10,15 @@ asignarTextoElemento('h1','Juego del numero secreto');
 asignarTextoElemento('p','Ingrese un numero del 1 al 10');
 
 function intentoDeUsuario(){
-    alert("click desde el boton")
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroaleatorio)
+    console.log(typeof(numeroaleatorio));
+    console.log(numeroDeUsuario)
+    console.log(numeroaleatorio === numeroDeUsuario);
+    return;
+}
+
+function generarnumeroaleatorio(){
+    return Math.floor(Math.random()*10) +1;
 }
