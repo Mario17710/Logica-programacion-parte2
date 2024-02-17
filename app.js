df16da3267348d6ee56efc3fbfdf05c165c9ba4e
Marios <marioqquelcca1998@gmail.com>
2024-02-17 17:52:38 -0500

    cambios

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 // variable global
 let numeroaleatorio = generarnumeroaleatorio();
-let intentos =1;
+let intentos = 1;
 // creamos la funcion donde creamos las variables
-function asignarTextoElemento(elemento, texto){
+function asignarTextoElemento(elemento, texto) {
     let elementohtml = document.querySelector(elemento);
     elementohtml.innerHTML = texto;
 }
-// llamamos a la funcion y modificamos el comportamiento de los elementos
-asignarTextoElemento('h1','Juego del numero secreto');
-asignarTextoElemento('p','Ingrese un numero del 1 al 10');
 
-function intentoDeUsuario(){
+function intentoDeUsuario() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    if(numeroDeUsuario == numeroaleatorio){
-        asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${(intentos ===1 ? 'vez' : 'Veces')}`);
-        document.getElementById('reiniciar').removeAttribute('disabled')
+    if (numeroDeUsuario == numeroaleatorio) {
+        asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos === 1 ? 'vez' : 'Veces')}`);
+        document.getElementById('reiniciar').removeAttribute('disabled') // elimina o activa el boton nuevo
     }
-    else{
-        if(numeroDeUsuario > numeroaleatorio){
-            asignarTextoElemento('p','El numero secreto es menor');
+    else {
+        if (numeroDeUsuario > numeroaleatorio) {
+            asignarTextoElemento('p', 'El numero secreto es menor');
         }
-        else{
-            asignarTextoElemento('p','El numero secreto es mayor');
+        else {
+            asignarTextoElemento('p', 'El numero secreto es mayor');
         }
     }
     intentos++
@@ -29,10 +26,31 @@ function intentoDeUsuario(){
     return;
 }
 // Funcion que limpia la caja
-function limpiarcaja(){
-    document.querySelector('#valorUsuario').value='';
+function limpiarcaja() {
+    document.querySelector('#valorUsuario').value = '';
 }
 
-function generarnumeroaleatorio(){
-    return Math.floor(Math.random()*10) +1;
+// funcion que genera el numero aleatorio
+function generarnumeroaleatorio() {
+    return Math.floor(Math.random() * 10) + 1;
 }
+
+function condicionesIniciales() {
+    // llamamos a la funcion y modificamos el comportamiento de los elementos
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', 'Ingrese un numero del 1 al 10');
+    numeroaleatorio = generarnumeroaleatorio();
+    intentos = 1;
+}
+
+function reiniciarjuego() {
+    // Limpiar la caja
+    limpiarcaja();
+    // Indicar mensaje de intervalo de numeros
+    // Generar el numero aleatorio
+    // Inicializar el numero de intentos
+    condicionesIniciales();
+    // Deshabilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true')
+}
+condicionesIniciales();
